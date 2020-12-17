bd29144cb18d8834f0201708cf4c58558ef381ff
Shruti <shrutie.bhattarai@gmail.com>
2020-12-17 13:04:11 +0545

    Updated Procfile-2

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: daphne Rental.wsgi:application --port $PORT --bind 0.0.0.0 -v2 
+web: gunicorn Rental.wsgi
--- posts/views.py
+++ posts/views.py
@@ -53,13 +53,12 @@ def upload(request):
     if request.method == "POST":
         uploaded_file = request.FILES["document"]
         fs = FileSystemStorage()
-        # name =
-        fs.save(uploaded_file.name, uploaded_file)
-    #     context["url"] = fs.url(name)
-    #     form = ClothesForm(request.POST)
-    #     if form.is_valid():
-    #         form.save()
-    #     return redirect("/")
-    # else:
-    #     form = ClothesForm()
+        name = fs.save(uploaded_file.name, uploaded_file)
+        context["url"] = fs.url(name)
+        form = ClothesForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+    else:
+        form = ClothesForm()
     return render(request, "upload.html")
