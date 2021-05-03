6f68521adc289e395afb8a3d076c9190e35bad2a
Shruti Bhattarai <shrutie.bhattarai@gmail.com>
2021-05-03 09:59:52 +0545

    Final Commit

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class Clothes(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     # pdf = models.FieldFile(upload_to='books/pdfs/')
