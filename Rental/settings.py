4a993c34ebf44d3af8c9d5e10fcae87205af7f70
Shruti Bhattarai <shrutie.bhattarai@gmail.com>
2021-05-05 10:03:08 +0545

    Heroku Deployment

--- Rental/settings.py
+++ Rental/settings.py
@@ -81,13 +81,13 @@ WSGI_APPLICATION = "Rental.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = '''{
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    }'''
-    {'default': dj_database_url.config(default='postgres://localhost')}
-#}
+    }
+    #{'default': dj_database_url.config(default='postgres://localhost')}
+}
 
 
 # Password validation
