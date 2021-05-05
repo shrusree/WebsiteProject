885e6077133d5aef8abfda7e665ac0358423ff48
Shruti Bhattarai <shrutie.bhattarai@gmail.com>
2021-05-05 10:00:59 +0545

    Deployment Error Fixed

--- Rental/settings.py
+++ Rental/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,12 +81,13 @@ WSGI_APPLICATION = "Rental.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+DATABASES = '''{
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+    }'''
+    {'default': dj_database_url.config(default='postgres://localhost')}
+#}
 
 
 # Password validation
